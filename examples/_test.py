780da70f5655cd1c5ace23409139b663d04f0183
Sharpz7 <adam.mcarthur62@gmail.com>
2024-11-26 06:01:40 +0000

    Added Parallel Testing

--- examples/_test.py
+++ examples/_test.py
@@ -1,28 +1,44 @@
 import os
 import subprocess
 
+import pytest
+
 BLACKLIST = ["custom_ai_and_config.py"]
 
 
-def test_example_scripts():
+# Dynamically collect scripts for testing
+def collect_scripts():
     examples_dir = os.path.dirname(__file__)
     module_dir = os.path.abspath(
         os.path.join(examples_dir, "..")
     )  # Assuming your module is one level up from the examples directory
 
-    # walk through all the examples
+    scripts = []
     for root, _, files in os.walk(examples_dir):
         for file in files:
             if file.endswith(".py") and file not in BLACKLIST:
-                # run the script
-                script_path = os.path.join(root, file)
-                try:
-                    env = os.environ.copy()
-                    env["PYTHONPATH"] = module_dir
-                    subprocess.run(
-                        ["python", script_path], check=True, env=env
-                    )
-                except subprocess.CalledProcessError as e:
-                    print(f"Error running script: {script_path}")
-                    print(f"Error message: {e}")
-                    assert False
+                scripts.append((os.path.join(root, file), module_dir))
+    return scripts
+
+
+# Dynamically create test functions for pytest
+scripts = collect_scripts()
+for i, (script_path, module_dir) in enumerate(scripts):
+    # test_name should be derived from the script name
+    test_name = f'test_{script_path.split("/")[-1].replace(".py", "")}'
+
+    def create_test(script_path, module_dir):
+        def test():
+            try:
+                env = os.environ.copy()
+                env["PYTHONPATH"] = module_dir
+                subprocess.run(["python", script_path], check=True, env=env)
+            except subprocess.CalledProcessError as e:
+                pytest.fail(
+                    f"Script failed: {script_path}\nError message: {e}"
+                )
+
+        return test
+
+    # Add the test function to the global scope
+    globals()[test_name] = create_test(script_path, module_dir)
